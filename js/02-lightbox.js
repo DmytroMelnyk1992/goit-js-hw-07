578f2df690d4e538a6690ac0c3c84a51373d6a96
Dmytro Melnyk <dmytro.melnyk.1992@gmail.com>
2022-08-18 15:00:17 +0300

    gallery 2 ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image lazyload" src="${preview}" alt="${description}" />
+</a>
+</div>
+`;
+  });
+  join("");
+}
+// Standalone variant
+
+new SimpleLightbox('.gallery a', {
+  disableRightClick: true,
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
